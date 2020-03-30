5e85dc93e71e14fa3bd964220f691f5bc8d4d254
avemike <fajny.michal.gajek@gmail.com>
2020-03-30 10:07:03 +0200

    initial project boilerplate

---
+++ src/main.js
@@ -0,0 +1,3 @@
+import { Character } from './classes/Character'
+import { Room } from './classes/Room'
+import { Canvas } from './classes/Canvas'
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+        
+      }
+    ]
+  }
+};
